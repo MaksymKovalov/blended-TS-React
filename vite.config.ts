bc081f058675c47c3af63bcdcf3507ad8b5d5851
starlei <starlei13@gmail.com>
2025-08-24 15:15:19 +0300

    add 20s tasks blended3

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // For GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/blended-TS-React/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true
